0d9222a5bbb212616923700359ae10045378edfb
legendaryPark <pbjkmn00@gmail.com>
2024-03-11 18:20:26 -0700

    updated App.js and started tests

---
+++ src/tests/Upvote.test.js
@@ -0,0 +1,9 @@
+// import { render, fireEvent } from "@testing-library/react";
+// import UpvoteButton from "../components/common/UpvoteButton/UpvoteButton";
+
+// test("toggle upvote selection", () => {
+//   const { getByRole } = screen.getByRole(<UpvoteButton />);
+//   const button = getByRole("button");
+//   fireEvent.click(button);
+//   // Expect some change in the DOM indicating the selection state
+// });
