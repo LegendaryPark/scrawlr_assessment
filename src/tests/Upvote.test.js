d197228eb9e0af310f6ca15266af5a4bc596d007
legendaryPark <pbjkmn00@gmail.com>
2024-03-12 21:34:49 -0700

    Created test for upvote page

--- src/tests/Upvote.test.js
+++ src/tests/Upvote.test.js
@@ -1,9 +1,41 @@
-// import { render, fireEvent } from "@testing-library/react";
-// import UpvoteButton from "../components/common/UpvoteButton/UpvoteButton";
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import { UpvoteProvider } from "../context/UpvoteContext";
+import UpvoteList from "../components/UpvoteList/UpvoteList";
 
-// test("toggle upvote selection", () => {
-//   const { getByRole } = screen.getByRole(<UpvoteButton />);
-//   const button = getByRole("button");
-//   fireEvent.click(button);
-//   // Expect some change in the DOM indicating the selection state
-// });
+jest.mock(
+  "../components/common/UpvoteButton/UpvoteButton",
+  () =>
+    ({ selected, onToggle }) =>
+      (
+        <button
+          data-testid={"upvote-button"}
+          className={selected ? "button-selected" : "button-not-selected"}
+          style={{ backgroundColor: selected ? "#E5E8FD" : "#F4F6F8" }}
+          onClick={onToggle}
+        >
+          {selected ? "Selected" : "Not Selected"}
+        </button>
+      )
+);
+
+test("should apply correct background color on selection", () => {
+  render(
+    <UpvoteProvider>
+      <UpvoteList listIndex={0} />
+    </UpvoteProvider>
+  );
+
+  const addButton = screen.getByRole("button", { name: "+" });
+  fireEvent.click(addButton);
+
+  const upvoteButton = screen.getByTestId("upvote-button");
+  expect(upvoteButton).toHaveTextContent("Not Selected");
+  expect(upvoteButton).toHaveStyle("background-color: #F4F6F8");
+
+  // Simulate clicking the upvote button
+  fireEvent.click(upvoteButton);
+  expect(upvoteButton).toHaveTextContent("Selected");
+  expect(upvoteButton).toHaveStyle("background-color: #E5E8FD");
+});
